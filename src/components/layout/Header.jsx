fa2241218f028d4a854ca3cafb66a224868dec7f
hvhvvvvvvhvh <mvisac599@gmail.com>
2026-05-26 15:01:21 +0500

    mobile responsive navbar added

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,17 +1,59 @@
-export default function Header(){
-return(
-<header className="header">
-<div className="topbar">
-<div className="socials">
+// export default function Header(){
+// return(
+// <header className="header">
+// <div className="topbar">
+// <div className="socials">
+// <img src="/twitterwhite.png" className=""/>
+// <img src="/facebook-app-round-white-icon.webp" className=""/>
+// <img src="/instagram-white-icon.webp" className=""/>
+// </div>
+// <img src="/logo.svg" className="logo"/>
+// <button className="order-btn">ORDER ONLINE</button>
+// </div>
+// <nav className="menu">
+// <a href="/">HOME</a><a href="/menu">OUR MENU</a><a href="/about">ABOUT</a><a>OFFERS</a><a>CONTACT</a>
+// </nav>
+// </header>
+// )}
+
+"use client";
+
+import { useState } from "react";
+import Link from "next/link";
+
+export default function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <header className="header">
+      <div className="topbar">
+        <div className="socials">
 <img src="/twitterwhite.png" className=""/>
 <img src="/facebook-app-round-white-icon.webp" className=""/>
 <img src="/instagram-white-icon.webp" className=""/>
-</div>
-<img src="/logo.svg" className="logo"/>
-<button className="order-btn">ORDER ONLINE</button>
-</div>
-<nav className="menu">
-<a href="/">HOME</a><a href="/menu">OUR MENU</a><a href="/about">ABOUT</a><a>OFFERS</a><a>CONTACT</a>
-</nav>
-</header>
-)}
+        </div>
+
+        <img src="/logo.svg" className="logo"/>
+
+        <button className="order-btn">ORDER ONLINE</button>
+
+        <div
+          className={`hamburger ${menuOpen ? "active" : ""}`}
+          onClick={() => setMenuOpen(!menuOpen)}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </div>
+
+      <nav className={`menu ${menuOpen ? "show" : ""}`}>
+        <Link href="/">HOME</Link>
+        <Link href="/menu">OUR MENU</Link>
+        <Link href="/about">ABOUT</Link>
+        <Link href="/">OFFERS</Link>
+        <Link href="/">CONTACT</Link>
+      </nav>
+    </header>
+  );
+}
